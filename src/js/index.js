ecfb5be5be5cc197ca7c3457e876e6e07f6d72d7
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-11-02 11:55:30 +0900

    ⚡️ 메뉴추가하기 기능 구현

---
+++ src/js/index.js
@@ -0,0 +1,69 @@
+const $ = (selector) => document.querySelector(selector);
+
+function App() {
+    //form tag 자동으로 전송되는 것 막기 
+    $('#espresso-menu-form').addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
+    const addMenuName = () => {
+        //사용자 입력값이 빈값일 때에
+        if($('#espresso-menu-name').value === ''){
+            alert('값을 입력해 주세요');
+            //뒷부분 실행되지 않기 위해 return 
+            return;
+        }
+        const $menuName = $('#espresso-menu-name').value;
+        const menuItemTemplate = (menuName) => {
+            return `
+            <li class="menu-list-item d-flex items-center py-2">
+                <span class="w-100 pl-2 menu-name">${menuName}</span>
+                <button
+                type="button"
+                class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                >
+                수정
+                </button>
+                <button
+                    type="button"
+                    class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                >
+                삭제
+                </button>
+            </li>
+            `;
+        };
+        $('#espresso-menu-list').insertAdjacentHTML(
+            "beforeend",menuItemTemplate($menuName)
+        );
+        //메뉴의 개수 세기 
+        const menuCount = $('#espresso-menu-list').querySelectorAll("li").length;
+
+        $(".menu-count").innerText = `총 ${menuCount}개`;
+
+        //input 안에 내용 초기화 
+        $("#espresso-menu-name").value = '';    
+            
+        
+    }
+
+    //확인버튼 클릭시 
+    $('#espresso-menu-submit-button').addEventListener("click", () => {
+        addMenuName();
+    });
+
+    // 메뉴의 입력 
+    $('#espresso-menu-name').addEventListener('keypress', (e) => {
+        if(e.key !== 'Enter'){
+            return;
+        }
+        if(e.key === 'Enter'){
+            addMenuName();
+        };
+        
+    
+    });
+
+}
+
+App();
